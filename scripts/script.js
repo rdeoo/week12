9908a77351fc54f8c71dd5b70d6a3d7dcdb67318
Rohan Deo <rohandeo307@gmail.com>
2023-03-29 14:08:44 -0700

    event handling on button

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,24 @@ const button = document.querySelector("#ClickMe");
 //2. define a function to alert user
 function alertuser() {
   alert("you clicked!");
+//   button.removeEventListener("click", alertuser);
 }
 //3.add the function
-button.addEventListener("click", alertuser);
+button.addEventListener("click", alertuser, {once:true});
+
+
+function changeBGPink()
+{
+    // document.body.style = "background-color:pink"
+    document.body.classList.add("pinkBg");
+}
+button.addEventListener("click", changeBGPink);
+
+function changeText()
+{
+    // if it says click me change to clicekd
+    // else change it to click me
+if(button.textContent === "Click me!"){button.textContent = "clicked!"}
+else{button.textContent = "Click me!"}
+}
+button.addEventListener("click", changeText);
