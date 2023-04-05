c6808ac0c93ecd9992ab872a4745c336d2291426
Rohan Deo <rohandeo307@gmail.com>
2023-04-05 12:30:39 -0700

    change text couler and add new elements

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,32 @@ function changeBGGreen(event)
 }
 //3 add func as event lsitener
 buttonContainer.addEventListener("click",changeBGGreen)
+
+// 2. define a functiuon to change textcolor
+function changeTextColor(event)
+{   if(event.target.tagName === "BUTTON")
+    {event.target.style.color = event.target.textContent;}
+    // if "red" button is clicked -> the text color should be read
+}
+
+// 3. event listner
+buttonContainer.addEventListener("click", changeTextColor)
+
+// 1. find the element
+// 2. define function
+// 3. addEventListener
+function addNewElements()
+{
+    // create new elements -> p and button
+    const newButton = document.createElement("button")
+    const newp = document.createElement("p")
+    newButton.addEventListener("click", changeBGGreen)
+    // appened chikd
+    document.body.appendChild(newButton)
+    document.body.appendChild(newp)
+    // add text content
+    newp.textContent ="Something"
+    newButton.textContent ="Purple"
+}
+// add event listener
+button.addEventListener("click", addNewElements)
